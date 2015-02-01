15c00a85efbb119db904c0e2a1410e4cbbf5c625
aarce <aarce@udel.edu>
2015-01-31 20:30:47 -0500

    all files

---
+++ PersonalWebsite(localvers).php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Abraham Arce</title>
+
+        <link type="text/css" rel="stylesheet" href="bootstrap-3.3.2-dist/css/bootstrap.css" />
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+
+<body>
+
+        <!-- Navigation Bar =========================================-->
+  <div class="navbar navbar-default" style="border-radius:0px; -webkit-border-radius:0px; -moz-border-radius:0px;">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="//abrahamarce.com">
+          <img alt="Brand" src="ADA_logo3.svg">
+        </a>
+      </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav pull-right">
+            <li> <a href="#">About</a></li>
+            <li> <a href="#">Projects</a></li>
+            <li> <a href="#">Contact</a></li>
+        </ul>
+
+
+
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="bootstrap-3.3.2-dist/js/bootstrap.js"></script>
+</body>
+</html>
+
+
---
+++ yogonzo.php
@@ -0,0 +1,86 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Yo Gonzo Pics</title>
+
+        <link type="text/css" rel="stylesheet" href="bootstrap-3.3.2-dist/css/bootstrap.css" />
+        <link type="text/css" rel="stylesheet" href="css/style.css" />
+        <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<div class="row text-center">
+	<a href="//www.yogonzopics.com"><img src="YoGonzoLogo.png" alt="YoGonzo" width="234" height="205"></a>
+</div> 
+  <div class="navbar navbar-default" style="border-radius:0px; -webkit-border-radius:0px; -moz-border-radius:0px;">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+      </div>
+      <div class="navbar-collapse collapse">
+        <ul class="nav navbar-nav center">
+          <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="0" role="button" aria-expanded="false">Gallery</a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="#">Portraits</a></li>
+            <li><a href="#">Photography</a><li>
+            <li><a href="#">Family Portraits</a><li>
+          </ul>
+            <li> <a href="#">Events</a></li>
+            <li> <a href="#">About</a></li>
+            <li> <a href="#">Contact</a></li>
+    
+        </ul>
+
+
+      </div>
+    </div>
+  </div>
+
+  <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="..." alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    <div class="item">
+      <img src="..." alt="...">
+      <div class="carousel-caption">
+        ...
+      </div>
+    </div>
+    ...
+  </div>
+
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+        <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="bootstrap-3.3.2-dist/js/bootstrap.js"></script>
+<script src="bootstrap-3.3.2-dist/js/bootstrap-hover-dropdown.js"></script>
+</body>
+</html>
